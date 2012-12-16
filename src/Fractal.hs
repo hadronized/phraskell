9ffe08715f320f2ab7be24da10553833c47563aa
Dimitri 'skp' Sabadie <dimitri.sabadie@gmail.com>
2012-12-16 14:49:17 +0100

    Added a helper function foreachPixel and aliased pixel type.

--- src/Fractal.hs
+++ src/Fractal.hs
@@ -12,10 +12,14 @@ mandelbrot x1 c = x1 ^ 2 + c
 evalFrac :: (FComplex -> FComplex -> FComplex) -> FComplex -> FComplex-> FComplex
 evalFrac e xy f = evalFrac (e) (e f xy) xy
 
-type Screen = [[(Float,Float)]]
+type Pixel = (Float,Float)
+type Screen = [[Pixel]]
 
 screen :: Float -> Float -> Screen
 screen w h = map (\x -> [ (x,y) | y <- [0..h-1] ]) [0..w-1]
 
-cartScreen :: Float -> Float -> Screen -> Screen
-cartScreen w h s = map (map (\(x,y) -> (2 * x / (w-1) - 1, 1 - 2 * y / (h-1)))) s
+foreachPixel :: Screen -> (Pixel -> Pixel) -> Screen
+foreachPixel s f = map ( map (\(x,y) -> f (x,y)) ) s
+
+toCart :: Float -> Float -> Screen -> Screen
+toCart w h s = foreachPixel s (\(x,y) -> (2 * x / (w-1) - 1, 1 - 2 * y / (h-1)))
