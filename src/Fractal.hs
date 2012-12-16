c2a69984814235e53e1a1b19c70b98faf4a11f8f
Dimitri 'skp' Sabadie <dimitri.sabadie@gmail.com>
2012-12-16 15:16:34 +0100

    Added some usefull functions to Fractal module.

--- src/Fractal.hs
+++ src/Fractal.hs
@@ -18,8 +18,16 @@ type Screen = [[Pixel]]
 screen :: Float -> Float -> Screen
 screen w h = map (\x -> [ (x,y) | y <- [0..h-1] ]) [0..w-1]
 
-foreachPixel :: Screen -> (Pixel -> Pixel) -> Screen
-foreachPixel s f = map ( map (\(x,y) -> f (x,y)) ) s
+foreachPixel = map . map
 
+-- convert a standard-coordinates screen into a cartesian-coordinates (origin-centered) one
 toCart :: Float -> Float -> Screen -> Screen
-toCart w h s = foreachPixel s (\(x,y) -> (2 * x / (w-1) - 1, 1 - 2 * y / (h-1)))
+toCart w h s = foreachPixel (\(x,y) -> (2 * x / (w-1) - 1, 1 - 2 * y / (h-1))) s
+
+-- fractal zoom, take a zoom factor (2.0 means to zoom in with a coef of 2), and zoom around the origin
+oZoom :: Float -> Screen -> Screen
+oZoom z s = foreachPixel (\(x,y) -> (x/z, y/z)) s
+
+-- fractal translation
+translate :: Float -> Float -> Screen -> Screen
+translate rx ry s = foreachPixel (\(x,y) -> (x+rx,y+ry)) s
