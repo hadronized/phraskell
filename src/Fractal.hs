68251ea7be5ed29c2fedbae411e41efcbb0a36f1
Dimitri 'skp' Sabadie <dimitri.sabadie@gmail.com>
2012-12-16 14:25:08 +0100

    Aliased Screen type.

--- src/Fractal.hs
+++ src/Fractal.hs
@@ -12,6 +12,10 @@ mandelbrot x1 c = x1 ^ 2 + c
 evalFrac :: (FComplex -> FComplex -> FComplex) -> FComplex -> FComplex-> FComplex
 evalFrac e xy f = evalFrac (e) (e f xy) xy
 
--- Iteration frame
-iterFrame :: Int -> Int -> [[(Int,Int)]]
-iterFrame w h = map (\x -> [ (x,y) | y <- [0..h-1] ]) [0..w-1]
+type Screen = [[(Float,Float)]]
+
+screen :: Float -> Float -> Screen
+screen w h = map (\x -> [ (x,y) | y <- [0..h-1] ]) [0..w-1]
+
+cartScreen :: Float -> Float -> Screen -> Screen
+cartScreen w h s = map (map (\(x,y) -> (2 * x / w - 1, - 2 * y / h))) s
