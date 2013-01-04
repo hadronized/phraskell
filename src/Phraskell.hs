9b279d933604bf06460bca1da20b750b0506d60a
Dimitri Sabadie <dimitri.sabadie@me.com>
2013-01-04 13:44:46 +0100

    Now try to get screen

--- src/Phraskell.hs
+++ src/Phraskell.hs
@@ -1,12 +1,17 @@
 import Fractal
 import Render
 
-main = do
-  screen <- initRender width height depth "Phraskell"
-
-  destroyRender
+main = withInit [InitVideo] $ do
+  screen <- tryGetScreen width height depth title
+  case screen of
+    Just s -> loop s
+    _      -> return ()
 
   where
     width  = 800
     height = 600
     depth  = 32
+    title  = "Phraskell"
+    
+    loop screen = do
+      -- first of all, handle events
