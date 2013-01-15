249cefa31f806a6ba48a3f7766ac6f7f6ca745d9
Dimitri 'skp' Sabadie <dimitri.sabadie@gmail.com>
2013-01-15 12:18:44 +0100

    Ready to support events!

--- src/Phraskell.hs
+++ src/Phraskell.hs
@@ -7,6 +7,13 @@ import Render
 import System.Environment
 import System.Console.GetOpt
 
+width,height,depth :: Float
+title :: String
+width  = 800
+height = 600
+depth  = 32
+title  = "Phraskell"
+
 -- TODO: add fullscreen support
 data App = App {
   appWidth :: Float,
@@ -31,6 +38,9 @@ data Flag
   | FRY String     -- relative y value
   | FZoom String   -- zoom value
 
+usage :: IO ()
+usage = putStrLn $ usageInfo "usage: phraskell [OPTIONS]" options
+
 options :: [OptDescr Flag]
 options =
   [ Option ['v','?'] ["version", "about"] (NoArg FVersion)           "show version"
@@ -47,67 +57,50 @@ parseOpts args =
     (o,n,[])   -> return $ Just (o,n)
     (_,_,errs) -> return Nothing
 
-initApp :: App -> ([Flag],[String]) -> Maybe AppState
+initApp :: App -> ([Flag],[String]) -> App
 initApp app (flags,_) = do
-    return $ do
-      put $ app `initWithFlags` flags
-      return True 
- where initWithFlags a f = foldl modifyAppWithOpt a f
+  app `initWithFlags` flags
+    where initWithFlags a f = foldl modifyAppWithOpt a f
 
 -- alter an application regarding an option flag
 modifyAppWithOpt :: App -> Flag -> App
 modifyAppWithOpt app f = case f of
-    FWidth s  -> app { appWidth = read s }
-    FHeight s -> app { appHeight = read s }
-    FRX s     -> app { appRX = read s }
-    FRY s     -> app { appRX = read s }
-    FZoom s   -> app { appZoom = read s }
-    _         -> app
+  FWidth s  -> app { appWidth = read s }
+  FHeight s -> app { appHeight = read s }
+  FRX s     -> app { appRX = read s }
+  FRY s     -> app { appRX = read s }
+  FZoom s   -> app { appZoom = read s }
+  _         -> app
 
 -- entry point
 main = do
-  -- get the options
-  args <- getArgs
-  maybeCliOpts <- parseOpts args
+  args <- getArgs -- get the CLI options
+  maybeCliOpts <- parseOpts args -- maybe get the options
   case maybeCliOpts of
-    Nothing -> return ()
-    Just x  -> withOpts x
-  putStrLn "Bye!"
-
-  where
-    withOpts cliOpts = do
-      -- create and init the application
-      let app = App width height 0 0 1 mandelbrotEquation []
-      maybeAppState <- lift $ initApp app cliOpts
-      case maybeAppState of
-        Nothing -> return ()
-        Just x  -> withAppState x
-
-    withAppState app = do
-      screen <- tryGetScreen width height depth title
-      case screen of
-        Just s -> loop app
-        _      -> return ()
-
-    loop a = do
-      newApp <- treatEvents a
-      let (quit,_) = runState newApp
-      unless quit $ loop newApp
-
-    width  = 800.0
-    height = 600.0
-    depth  = 32.0
-    title  = "Phraskell"
+    Nothing -> usage
+    Just flags  -> do
+      -- here we have options, so let’s create our very first application ! but before, create the screen
+      withInit [InitVideo] $ do
+        maybeScreen <- trySetVideoMode (floor width) (floor height) (floor depth) [HWSurface,DoubleBuf]
+        case maybeScreen of
+          Nothing -> putStrLn "unable to get a screen! :("
+          Just screen -> do
+            let app = initApp (App width height 0.0 0.0 1.0 mandelbrotEquation []) flags -- the application
+            loop app
+            putStrLn "Bye!"
+              where loop app = do
+                      (quit,newApp) <- treatEvents app
+                      unless quit $ loop newApp
 
 -- events handler
-treatEvents :: AppState -> IO AppState
+treatEvents :: App -> IO (Bool,App)
 treatEvents app = do
   event <- waitEvent
   case event of
-    NoEvent  -> return False
-    Quit     -> return True
+    NoEvent  -> return $ (False,app)
+    Quit     -> return $ (False,app)
     KeyUp k  -> case symKey k of
-       SDLK_ESCAPE -> app >>= return True
-       SDLK_RETURN -> app >>= return True
-       _           -> treatEvents app
+      SDLK_ESCAPE -> return $ (True,app)
+      SDLK_RETURN -> return $ (True,app)
+      _           -> treatEvents app
     _        -> treatEvents app
