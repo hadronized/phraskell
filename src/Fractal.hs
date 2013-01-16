761cd8877768f69a116dadb5e584e01fe09acae9
Dimitri 'skp' Sabadie <dimitri.sabadie@gmail.com>
2013-01-16 21:25:04 +0100

    Code convention.

--- src/Fractal.hs
+++ src/Fractal.hs
@@ -8,10 +8,10 @@ type Equation = (FComplex -> FComplex -> FComplex)
 -- for x and y, evaluate the fractal equation
 evalFrac :: Equation -> FComplex -> FComplex -> Integer -> Integer
 evalFrac e xy f m = go xy f 0
-    where go xy f i
-            | i >= m = m
-            | realPart (abs xy) > 2.0 = i
-            | otherwise = go (e f xy) xy (i+1)
+  where go xy f i
+          | i >= m = m
+          | realPart (abs xy) > 2.0 = i
+          | otherwise = go (e f xy) xy (i+1)
 
 type Pixel = (Float,Float)
 type Frame = [[Pixel]]
@@ -34,7 +34,7 @@ type IterFrame = [[Integer]]
 -- take w, h, rx, ry, z and the fractal equation
 mkIterFrame :: Float -> Float -> Float -> Float -> Float -> Equation -> IterFrame
 mkIterFrame w h rx ry z e =
-    let frame = (map . map $ (offsets rx ry) . (oZoom z) . (toCart w h ratio)) $ screen w h
-        eval  = (map . map $ (\(x,y) -> (evalFrac e (x :+ y) (fromInteger 0) 50)))
-        ratio = w / h
-    in eval frame
+  let frame = (map . map $ (offsets rx ry) . (oZoom z) . (toCart w h ratio)) $ screen w h
+      eval  = (map . map $ (\(x,y) -> (evalFrac e (x :+ y) (fromInteger 0) 50)))
+      ratio = w / h
+  in eval frame
