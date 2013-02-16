f8c2202fecbe032c3446aad5a540bf1f6a9cbb2f
Dimitri 'skp' Sabadie <dimitri.sabadie@gmail.com>
2013-02-16 13:01:13 +0100

    Added first fractal controller.

---
+++ src/Controller/FractalController.hs
@@ -0,0 +1,11 @@
+module Controller.FractalController where
+
+data FractalController = FractalController {
+    progression :: FractalProgression
+  , model       :: FractalModel
+  , view        :: FractalView
+}
+
+runFractalCtrl :: FractalController -> IO ()
+runFractalCtrl frac = do
+  runFractalView view model
