e705990b5bcbb18dd97bc8a967d5ec8220b5fc5a
Dimitri 'skp' Sabadie <dimitri.sabadie@gmail.com>
2013-02-17 17:59:15 +0100

    Implementing gui controller.

--- src/Controller/GUIController.hs
+++ src/Controller/GUIController.hs
@@ -1,3 +1,13 @@
 module Controller.GUIController where
 
+import Graphics.UI.SDL
+
+data GUIController = GUIController {
+    guiCtrlVisible  :: Bool
+  , guiCtrlZoomArea :: Surface
+}
+
+createGUICtrl :: 
+runGUICtrl :: GUIController -> IO ()
+runGUICtrl gui = do
 
