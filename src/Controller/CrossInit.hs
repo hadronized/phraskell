cc6dc7c91500e3dccaed8b81df0af2ef080c9308
Dimitri 'skp' Sabadie <dimitri.sabadie@gmail.com>
2013-02-18 23:20:01 +0100

    Added CrossInit module.

---
+++ src/Controller/CrossInit.hs
@@ -0,0 +1,36 @@
+module CrossInit where
+
+import Control.Monad.Trans.Maybe
+import Controller.Bootstrap
+
+title :: String
+title = "phraskell 0.1.3"
+
+crossInit :: Bootstrap -> MaybeT IO AppController
+crossInit b = do
+  lift SDL.init [InitVideo] -- first thing to do, init SDL
+  let w           = bootWidth b
+      h           = bootHeight b
+      progression = mandelbrot--bootProgression b
+      zf          = 0.5
+  -- AppController init: acquiring a screen Surface
+  screen <- tryGetScreen w h 32 title
+  -- AppController init: building the FractalController
+  --   FractalController init: building the view
+  --     FractalView init: acquiring a surface
+  fractalFrame <- tryCreateSurface w h 32
+  -- AppController init: building the GUIController
+  --   GUIController init: building the view
+  --     GUIView init: acquiring a surface
+  zoomWindow <- tryCreateZoomWindow w h zf
+  
+tryCreateSurface :: Int -> Int -> Int -> MaybeT IO Surface
+tryCreateSurface w h d = MaybeT $ SDL.tryCreateRGBSurface [HWSurface] w h d 0 0 0 0
+
+tryGetScreen :: Int -> Int -> Int -> String -> MaybeT IO Surface
+tryGetScreen w h d t = do
+  screen <- MaybeT $ SDL.trySetVideoMode w h d [HWSurface, DoubleBuf]
+  SDL.setCaption t [] -- we don’t give a fuck about the title icon
+  return screen
+
+
