5765fa61d6441fdd047c0158371014475601e921
Dimitri 'skp' Sabadie <dimitri.sabadie@gmail.com>
2013-02-25 00:04:29 +0100

    Working of FFI.

--- src/Controller/Fractal.hs
+++ src/Controller/Fractal.hs
@@ -1,5 +1,11 @@
-module Controller.Fractal where
+module Controller.Fractal 
+  (
+    module Controller.Fractal.Hard
+  , ModelProcessor(..)
+  , computeModel
+  ) where
 
+import Controller.Fractal.Hard
 import Model.Fractal
 import Model.Progression
 import View.Fractal
@@ -7,7 +13,7 @@ import View.Fractal
 -- ModelProcessor, used to determine how to run the model computation.
 data ModelProcessor
   = SoftModelProcessor
-  -- | HardModelProcessor ????
+  | HardModelProcessor ShaderProgram
 
 computeModel :: ModelProcessor -> FractalModel -> FractalProgression -> Double -> Double -> Double -> Double -> Double -> Integer -> IO FractalModel
 computeModel SoftModelProcessor m p w h x y z mi = case m of
--- src/Controller/Fractal/Hard.hs
+++ src/Controller/Fractal/Hard.hs
@@ -1,31 +1,53 @@
 module Controller.Fractal.Hard where
 
+import Control.Monad
+import Control.Monad.Trans
 import Control.Monad.Trans.Maybe
+import Data.ByteString
+import Foreign.Storable
 import Foreign.C.String
+import Foreign.Marshal.Alloc
 import Graphics.Rendering.OpenGL.Raw
 
 type ShaderProgram = GLuint
+type ShaderStage   = GLuint
 
 vertexShaderStr,fragmentShaderStr :: String
-vertexShaderStr = "#version 150\n\
-in vec2 co;\n\
-out vec2 cardco;\n\
+vertexShaderStr   = "#version 150\n\
+                    \in vec2 co;\n\
+                    \out vec2 cardco;\n\
 
-void main() {\n\
-  gl_Position = co;\n\
-  cardco = co; /* TODO: add offsets and zoom support */
-}"
+                    \void main() {\n\
+                    \  gl_Position = co;\n\
+                    \  cardco = co; /* TODO: add offsets and zoom support */ \n\
+                    \}"
 fragmentShaderStr = "#version 150\n\
-in vec2 cardco;\n\
+                    \in vec2 cardco;\n\
+                    \out vec4 frag;\n\
 
-void main() {\n\
-}"
+                    \void main() {\n\
+                    \  frag = vec4(0.f, 0.f, 0.f, 1.f);\n\
+                    \}"
 
+{-
 createShaderProgram :: MaybeT IO ShaderProgram
 createShaderProgram = do
   vertexShader <- lift $ glCreateShader gl_VERTEX_SHADER
-  guard vertexShader == 0
-  fragmentShader <- lift $ glCreateShader gl_FRAGMENT_SHADER
-  guard fragmentShader == 0
+  guard $ vertexShader == 0
 
-  glShaderSource vertexShader (fromIntegral 1) 
+-}
+-- glShaderSource :: GLuint -> GLsizei -> Ptr (Ptr GLchar) -> Ptr GLint -> IO ()
+
+createShaderStage :: GLenum -> MaybeT IO ShaderStage
+createShaderStage st = do
+  stage <- lift $ glCreateShader st
+  guard $ stage /= 0
+  return stage
+
+compileShaderStage :: ShaderStage -> ByteString -> MaybeT IO ()
+compileShaderStage s src = do
+  nbPtr <- lift $ malloc
+  lift $ poke nbPtr (fromIntegral 1 :: Int)
+  csrc <- lift $ newCString src
+  --glShaderSource s (fromIntegral 1) {- src -} {- number of src -}
+  return ()
