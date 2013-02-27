450c81f2cf91c4e0e2996636abb1545f8b99c659
Dimitri 'skp' Sabadie <dimitri.sabadie@gmail.com>
2013-02-27 11:55:15 +0100

    Implementing shader program.

--- src/Controller/Fractal/Hard.hs
+++ src/Controller/Fractal/Hard.hs
@@ -3,12 +3,14 @@ module Controller.Fractal.Hard where
 import Control.Monad
 import Control.Monad.Trans
 import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Either
 import Foreign
 import Foreign.C.String
 import Graphics.Rendering.OpenGL.Raw
 
 type ShaderProgram = GLuint
 type ShaderStage   = GLuint
+type ShaderProgram = GLuint
 
 type GLError = GLenum
 
@@ -19,9 +21,22 @@ fromGLError e = case e of
   gl_INVALID_VALUE     -> "invalid value"
   gl_INVALID_OPERATION -> "invalid operation"
   gl_OUT_OF_MEMORY     -> "out of memory"
-  _                    -> "unkown error"
+  _                    -> "unknown error"
 
---validateGL :: 
+{-
+newtype IOGL a = IOGL { runIOGL :: IO (Either GLError a) }
+
+instance Monad IOGL where
+  return = IOGL . return . return
+  IOGL glio >>= f = IOGL $ do
+    v <- glio
+    case v of
+      Left  e -> return $ Left e
+      Right x -> do
+        y   <- runIOGL $ f x
+        err <- glGetError
+        if err /= gl_NO_ERROR then return $ Left err else return y
+-}
 
 vertexShaderStr,fragmentShaderStr :: String
 vertexShaderStr   = "#version 150\n\
@@ -40,38 +55,41 @@ fragmentShaderStr = "#version 150\n\
                     \  frag = vec4(0.f, 0.f, 0.f, 1.f);\n\
                     \}"
 
-{-
-createShaderProgram :: MaybeT IO ShaderProgram
+createShaderProgram :: EitherT IO String ShaderProgram
 createShaderProgram = do
-  vertexShader <- lift $ glCreateShader gl_VERTEX_SHADER
-  guard $ vertexShader == 0
+  vs <- EitherT $ createCompileShaderStage gl_VERTEX_SHADER vertexShaderStr
+  fs <- Eithert $ createCompileShaderStage gl_FRAGMENT_SHADER fragmentShaderStr
 
--}
--- glShaderSource :: GLuint -> GLsizei -> Ptr (Ptr GLchar) -> Ptr GLint -> IO ()
+createCompileShaderStage :: GLenum -> String -> IO (Either String ShaderStage)
+createCompileShaderStage st src = do
+  ms <- createShaderStage st
+  case ms of
+    Nothing -> return $ Left "unable to create shader stage"
+    Just s -> do
+      compileShaderStage s src
+      compiled <- checkCompilation s
+      if not compiled
+        then compilationLog s >>= return . Left
+        else do
+          return $ Right s
 
 createShaderStage :: GLenum -> IO (Maybe ShaderStage)
 createShaderStage st = do
   stage <- glCreateShader st
   return $ if stage /= 0 then Just stage else Nothing
 
-compileShaderStage :: ShaderStage -> String -> IO Bool
+compileShaderStage :: ShaderStage -> String -> IO ()
 compileShaderStage s src = do
   with 1 $ \nb -> do
     foo <- newCString src
     with foo  $ \csrc -> do
       glShaderSource s (fromIntegral 1) (castPtr csrc) nb
-      return True
 
-checkCompilation :: ShaderStage -> IO (Maybe String)
+checkCompilation :: ShaderStage -> IO Bool
 checkCompilation s = do
   with 0 $ \status -> do
     glGetShaderiv s gl_COMPILE_STATUS status
-    compiled <- toBool `liftM` peek status
-    if compiled == True
-      then return Nothing
-      else do
-      l <- compilationLog s
-      return $ Just l
+    toBool `liftM` peek status
 
 compilationLog :: ShaderStage -> IO String
 compilationLog s = do
@@ -82,3 +100,15 @@ compilationLog s = do
       glGetShaderInfoLog s (fromIntegral bytes) nullPtr linfo
       str <- peekCString (castPtr linfo)
       return str
+
+createProgram :: IO (Maybe ShaderProgram)
+createProgram = do
+  sp <- glCreateProgram
+  return $
+
+attachToProgram :: ShaderProgram -> ShaderStage -> IO ()
+attachToProgram = glAttachShader
+
+linkProgram :: ShaderProgram -> IO ()
+linkProgram sp = do
+  glLinkProgram sp
